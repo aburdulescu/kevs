6419a773df85bcb19f666faf37c27523b5a127e2
Andrei Burdulescu <andrei.burdulescu@pm.me>
2024-12-27 21:40:50 +0200

    Fix trace script

--- trace.sh
+++ trace.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-sudo perf probe -q -x ./kevs --add="scan_*" --add="scan_*%return"
-sudo perf probe -q -x ./kevs --add="parse_*" --add="parse_*%return"
-sudo perf record -e "probe_kevs:*" ./kevs $@ || true
+sudo perf probe -q -x ./b/kevs --add="scan_*" --add="scan_*%return"
+sudo perf probe -q -x ./b/kevs --add="parse_*" --add="parse_*%return"
+sudo perf record -e "probe_kevs:*" ./b/kevs $@ || true
 sudo perf probe -q --del "probe_kevs:*"
 sudo perf script -s trace.py
