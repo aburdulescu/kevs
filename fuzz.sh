9bfb89c676674c234363ba01f707cc3b8722a9d1
Andrei Burdulescu <andrei.burdulescu@pm.me>
2024-12-30 10:20:08 +0200

    Update fuzzer script

--- fuzz.sh
+++ fuzz.sh
@@ -4,8 +4,8 @@ set -ex
 
 # add testdata to corpus
 mkdir -p testdata/corpus
-./b/kevs_fuzz -detect_leaks=0 -merge=1 testdata/corpus/ testdata/not_valid/
-./b/kevs_fuzz -detect_leaks=0 -merge=1 testdata/corpus/ testdata/valid/
+./b/kevs_fuzz -merge=1 testdata/corpus/ testdata/not_valid/
+./b/kevs_fuzz -merge=1 testdata/corpus/ testdata/valid/
 
 # create temp corpus dir
 rm -rf corpus
@@ -13,9 +13,14 @@ mkdir corpus
 cp -r testdata/corpus/* corpus/
 
 # run fuzzer
-./b/kevs_fuzz -detect_leaks=0 -max_total_time=60 corpus
+./b/kevs_fuzz -max_total_time=10 corpus
+
+# generate coverage
+rm -rf coverage.data coverage-out
+./cov-merge.py default.profraw -o coverage.data
+./cov-show.py b/kevs_fuzz -p coverage.data -s kevs.c
 
 # merge corpus
 rm -rf testdata/corpus
 mkdir -p testdata/corpus
-./b/kevs_fuzz -detect_leaks=0 -merge=1 testdata/corpus/ corpus
+./b/kevs_fuzz -merge=1 testdata/corpus/ corpus
