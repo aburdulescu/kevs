ee0ab223d24cc9d95e3ae993459aa553a97c4965
Andrei Burdulescu <andrei.burdulescu@pm.me>
2024-12-31 17:23:56 +0200

    Update test runner

--- scripts/tests.go
+++ scripts/tests.go
@@ -105,39 +105,109 @@ func runValid() error {
 	return nil
 }
 
+func runNotValid() error {
+	var tests []Test
+	err := filepath.WalkDir("testdata/not_valid", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+		if !strings.HasSuffix(path, ".kevs") {
+			return nil
+		}
+
+		name := strings.TrimSuffix(path, ".kevs")
+		expected := name + ".out"
+
+		if !*update {
+			if _, err := os.Stat(expected); err != nil {
+				return fmt.Errorf("cannot find file with expected output for '%s'", path)
+			}
+		}
+
+		tests = append(tests, Test{
+			name:     name,
+			input:    path,
+			expected: expected,
+		})
+
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	var total time.Duration
+	var nfailed int
+
+	w := tabwriter.NewWriter(os.Stdout, 0, 2, 2, ' ', 0)
+
+	for _, test := range tests {
+		fmt.Fprintf(w, "%s\t", test.name)
+		start := time.Now()
+		err := test.run()
+		end := time.Now()
+		if err != nil {
+			fmt.Fprint(w, "failed")
+			nfailed++
+		} else {
+			fmt.Fprint(w, "passed")
+		}
+		e := end.Sub(start)
+		total += e
+		fmt.Fprintf(w, "\t%s\n", e)
+	}
+
+	w.Flush()
+
+	fmt.Printf("summary: ran %d tests in %s, %d failed\n", len(tests), total, nfailed)
+
+	if nfailed != 0 {
+		return fmt.Errorf("tests failed")
+	}
+
+	return nil
+}
+
 type Test struct {
 	name     string
 	input    string
 	expected string
+	out      bytes.Buffer
 }
 
 func (t Test) run() error {
 	exe := filepath.Join(*buildDir, "kevs")
-	out := new(bytes.Buffer)
 	cmd := exec.Command(exe, "-abort", "-dump", t.input)
-	cmd.Stdout = out
+	cmd.Stdout = &t.out
 	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("failed to run command: %w", err)
 	}
 	if *update {
-		err := os.WriteFile(t.expected, out.Bytes(), 0600)
+		err := os.WriteFile(t.expected, t.out.Bytes(), 0600)
 		if err != nil {
 			return fmt.Errorf("failed to write output file: %w", err)
 		}
-	} else {
-		data, err := os.ReadFile(t.expected)
-		if err != nil {
-			return fmt.Errorf("failed to read expected output file: %w", err)
-		}
-		haveLines := strings.Split(out.String(), "\n")
-		wantLines := strings.Split(string(data), "\n")
-		if len(haveLines) != len(wantLines) {
-			return fmt.Errorf("error: want %d lines, have %d lines\n", len(wantLines), len(haveLines))
-		}
-		for i := range wantLines {
-			if haveLines[i] != wantLines[i] {
-				return fmt.Errorf("error: line %d: want '%s', have '%s'\n", i+1, wantLines[i], haveLines[i])
-			}
+	}
+	return t.checkValid()
+}
+
+func (t Test) checkValid() error {
+	data, err := os.ReadFile(t.expected)
+	if err != nil {
+		return fmt.Errorf("failed to read expected output file: %w", err)
+	}
+	haveLines := strings.Split(t.out.String(), "\n")
+	wantLines := strings.Split(string(data), "\n")
+	if len(haveLines) != len(wantLines) {
+		return fmt.Errorf("error: want %d lines, have %d lines\n", len(wantLines), len(haveLines))
+	}
+	for i := range wantLines {
+		if haveLines[i] != wantLines[i] {
+			return fmt.Errorf("error: line %d: want '%s', have '%s'\n", i+1, wantLines[i], haveLines[i])
 		}
 	}
 	return nil
