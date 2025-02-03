72dbf22c0d1edf1cba8d7c0ab3ee7fe2d0b49d49
Andrei Burdulescu <andrei.burdulescu@pm.me>
2025-02-03 22:15:31 +0200

    Fix fuzzer

--- src/fuzzer.c
+++ src/fuzzer.c
@@ -1,24 +1,26 @@
+#include <stdlib.h>
+
 #include "kevs.h"
 
+const size_t tables_buf_len = 5000 * sizeof(KeyValue);
+const size_t lists_buf_len = 5000 * sizeof(Value);
+const size_t tokens_buf_len = 5000 * sizeof(Token);
+const size_t strings_buf_len = 16 << 10;
+
+static char err_buf[8193] = {};
+
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-  const size_t tables_buf_len = 100;
-  KeyValue tables_buf[tables_buf_len];
-
-  const size_t lists_buf_len = 100;
-  Value lists_buf[lists_buf_len];
-
-  const size_t tokens_buf_len = 1000;
-  Token tokens_buf[tokens_buf_len];
-
-  char strings_buf[16 << 10];
+  KeyValue *tables_buf = malloc(tables_buf_len);
+  Value *lists_buf = malloc(lists_buf_len);
+  Token *tokens_buf = malloc(tokens_buf_len);
+  char *strings_buf = malloc(strings_buf_len);
 
   Allocators alls = {};
-  arena_init(&alls.tables, tables_buf, tables_buf_len * sizeof(tables_buf[0]));
-  arena_init(&alls.lists, lists_buf, lists_buf_len * sizeof(lists_buf[0]));
-  arena_init(&alls.tokens, tokens_buf, tokens_buf_len * sizeof(tokens_buf[0]));
-  arena_init(&alls.strings, strings_buf, sizeof(strings_buf));
 
-  char err_buf[8193] = {};
+  arena_init(&alls.tables, tables_buf, tables_buf_len);
+  arena_init(&alls.lists, lists_buf, lists_buf_len);
+  arena_init(&alls.tokens, tokens_buf, tokens_buf_len);
+  arena_init(&alls.strings, strings_buf, strings_buf_len);
 
   Table t = {};
 
@@ -31,6 +33,11 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   };
 
   table_parse(&t, params);
+
+  free(tables_buf);
+  free(lists_buf);
+  free(tokens_buf);
+  free(strings_buf);
 
   return 0;
 }
