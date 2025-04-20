3a85035df9c9901c0df90d0e89ea065331b2f237
Andrei Burdulescu <andrei.burdulescu@pm.me>
2025-04-20 11:41:56 +0300

    more work on Go implementation

--- kevs.go
+++ kevs.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	if err := mainErr(); err != nil {
-		fmt.Fprintln(os.Stderr, "error:", err)
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
@@ -163,7 +163,7 @@ func (self *scanner) advance(n int) {
 func (self *scanner) scan_comment() bool {
 	newline := strings.IndexByte(self.params.Content, '\n')
 	if newline == -1 {
-		self.scan_errorf("comment does not end with newline")
+		self.errorf("comment does not end with newline")
 		return false
 	}
 	self.advance(newline)
@@ -185,15 +185,199 @@ func (self *scanner) scan_key_value() bool {
 	return true
 }
 
-func (self *scanner) scan_errorf(format string, args ...any) {
-	self.err = fmt.Errorf("%s:%d: error: scan: %s", self.params.File, self.line, fmt.Sprintf(format, args))
+func (self *scanner) errorf(format string, args ...any) {
+	self.err = fmt.Errorf("%s:%d: error: scan: %s", self.params.File, self.line, fmt.Sprintf(format, args...))
+}
+
+func indexAny(s, chars string) (rune, int) {
+	i := strings.IndexAny(s, chars)
+	if i == -1 {
+		return 0, -1
+	}
+	for _, r := range chars {
+		i := strings.IndexRune(s, r)
+		if i != -1 {
+			return r, i
+		}
+	}
+	return 0, -1
 }
 
 func (self *scanner) scan_key() bool {
+	c, end := indexAny(self.params.Content, "=\n")
+	if end == -1 || c != kKeyValSep {
+		self.errorf("key-value pair is missing separator")
+		return false
+	}
+	self.append(tokenKindKey, end)
+	if len(self.tokens[len(self.tokens)-1].value) == 0 {
+		self.errorf("empty key")
+		return false
+	}
 	return true
 }
 
 func (self *scanner) scan_value() bool {
+	self.trim_space()
+	ok := false
+	if self.expect(kListBegin) {
+		ok = self.scan_list_value()
+	} else if self.expect(kTableBegin) {
+		ok = self.scan_table_value()
+	} else if self.expect(kStringBegin) {
+		ok = self.scan_string_value()
+	} else if self.expect(kRawStringBegin) {
+		ok = self.scan_raw_string()
+	} else {
+		ok = self.scan_int_or_bool_value()
+	}
+	if !ok {
+		return false
+	}
+	if !self.scan_delim(kKeyValEnd) {
+		self.errorf("value does not end with semicolon")
+		return false
+	}
+	return true
+}
+
+func (self *scanner) scan_delim(c byte) bool {
+	if !self.expect(c) {
+		return false
+	}
+	self.append_delim()
+	return true
+}
+
+func (self *scanner) scan_string_value() bool {
+	// advance past leading quote
+	s := self.params.Content[1:]
+
+	for {
+		// search for trailing quote
+		i := strings.IndexByte(s, kStringBegin)
+
+		if i == -1 {
+			self.errorf("string value does not end with quote")
+			return false
+		}
+
+		// get previous char
+		prev := s[i-1]
+
+		// advance
+		s = s[i+1:]
+
+		// stop if quote is not escaped
+		if prev != '\\' {
+			break
+		}
+	}
+
+	// calculate the end, includes trailing quote
+	end := len(s)
+
+	self.append(tokenKindValue, end)
+
+	return true
+}
+
+func (self *scanner) scan_raw_string() bool {
+	end :=
+		strings.IndexByte(self.params.Content[1:], kRawStringBegin)
+	if end == -1 {
+		self.errorf("raw string value does not end with backtick")
+		return false
+	}
+
+	// +2 for leading and trailing quotes
+	self.append(tokenKindValue, end+2)
+
+	// count newlines in raw string to keep line count accurate
+	self.line += strings.Count(self.tokens[len(self.tokens)-1].value, "\n")
+
+	return true
+}
+
+func (self *scanner) scan_int_or_bool_value() bool {
+	// search for all possible value endings
+	// if semicolon(or none of them) is not found => error
+	c, end := indexAny(self.params.Content, ";]}\n")
+	if end == -1 || c != kKeyValEnd {
+		self.errorf("integer or boolean value does not end with semicolon")
+		return false
+	}
+	self.append(tokenKindValue, end)
+	return true
+}
+
+func (self *scanner) scan_list_value() bool {
+	self.append_delim()
+	for {
+		self.trim_space()
+		if len(self.params.Content) == 0 {
+			self.errorf("end of input without list end")
+			return false
+		}
+		if self.expect('\n') {
+			if !self.scan_newline() {
+				return false
+			}
+			continue
+		}
+		if self.expect(kCommentBegin) {
+			if !self.scan_comment() {
+				return false
+			}
+			continue
+		}
+		if self.expect(kListEnd) {
+			self.append_delim()
+			return true
+		}
+		if !self.scan_value() {
+			return false
+		}
+		if self.expect(kListEnd) {
+			self.append_delim()
+			return true
+		}
+	}
+	return true
+}
+
+func (self *scanner) scan_table_value() bool {
+	self.append_delim()
+	for {
+		self.trim_space()
+		if len(self.params.Content) == 0 {
+			self.errorf("end of input without table end")
+			return false
+		}
+		if self.expect('\n') {
+			if !self.scan_newline() {
+				return false
+			}
+			continue
+		}
+		if self.expect(kCommentBegin) {
+			if !self.scan_comment() {
+				return false
+			}
+			continue
+		}
+		if self.expect(kTableEnd) {
+			self.append_delim()
+			return true
+		}
+		if !self.scan_key_value() {
+			return false
+		}
+		if self.expect(kTableEnd) {
+			self.append_delim()
+			return true
+		}
+	}
 	return true
 }
 
@@ -204,4 +388,17 @@ func (self *scanner) append_delim() {
 		line:  self.line,
 	})
 	self.advance(1)
+}
+
+func (self *scanner) append(kind tokenKind, end int) {
+	val := self.params.Content[:end]
+	val = strings.TrimRight(val, spaces)
+
+	self.tokens = append(self.tokens, token{
+		kind:  kind,
+		value: val,
+		line:  self.line,
+	})
+
+	self.advance(end)
 }
